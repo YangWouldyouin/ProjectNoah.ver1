7060c76d5ba42f16216178955aca90df849dc12e
SongChea <cogus4867@gmail.com>
2022-04-03 15:10:56 +0900

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,13 @@ public class GameManager : MonoBehaviour
     public bool IsHealthMachineFixedT_C2 = false; // 정기적으로 상태 측정 임무 수행 가능
     public bool IsSmartFarmOpen_T_C2 = false; // 항상 스마트팜 열려있고, 정기적으로 스마트팜 임무 수행 가능
 
+    //엔진실 카드키가 들어있는 카드팩이 파괴
+    public bool IsDisappearPack_M_C2 = false; 
+    // 엔진실 해금
+    public bool IsOpenCabinetDoor_M_C2 = false;
+    public bool IsFindRubbe_M_C2 = false;
+    public bool IsEngineDoorFix_M_C2 = false;
+    public bool IsEngineRoomOpen_M_C2 = false;
 
     /* 정기 임무 ( 일정 시간이 지나면 다시 false 로 바뀌어서 다시 해야함 ) */
     public bool IsHealthMachineDone = false; // 상태 체크 업무
