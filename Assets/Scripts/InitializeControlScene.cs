58d75ed3cf915030fe1fdc489ca451954e206768
skqlwldi <97214390+skqlwldi@users.noreply.github.com>
2022-05-09 03:53:22 +0900

    씬 저장값_이지예

--- Assets/Scripts/InitializeControlScene.cs
+++ Assets/Scripts/InitializeControlScene.cs
@@ -6,6 +6,19 @@ public class InitializeControlScene : MonoBehaviour
 {
     public Animator controlWorkDoorAnim;
     public GameObject ChangeScene;
+
+    [Header("<AI다운>")]
+    public GameObject chipInsert;
+    public GameObject RChip;
+    public GameObject WChip;
+
+    //콜라이더
+    BoxCollider chipInsertCol;
+    BoxCollider RChipCol;
+    BoxCollider WChipCol;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +29,17 @@ public class InitializeControlScene : MonoBehaviour
             controlWorkDoorAnim.SetBool("IsDoorOpenStart", true);
             controlWorkDoorAnim.SetBool("IsDoorOpened", true);
             ChangeScene.SetActive(true);
+        }
+
+        if(intialGameData.IsAIDown_M_C1C3)
+        {
+            chipInsertCol.enabled = false;
+            RChip.SetActive(true);
+            RChipCol.enabled = false;
+
+            RChip.transform.localScale = new Vector3(15.89634f, 15.89634f, 2.835073f);
+            RChip.transform.position = new Vector3(-7.448f, 34.62f, -1.439f);
+            RChip.transform.rotation = Quaternion.Euler(90, 0, 0);
         }
     }
 
