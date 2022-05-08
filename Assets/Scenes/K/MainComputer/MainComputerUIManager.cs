b8c288aed84a3189e21917e11d8bcedf7c2749b6
LoveJoo <97190841+LoveJoo@users.noreply.github.com>
2022-05-09 02:54:17 +0900

    K.MainCom UI 수정

--- Assets/Scenes/K/MainComputer/MainComputerUIManager.cs
+++ Assets/Scenes/K/MainComputer/MainComputerUIManager.cs
@@ -9,7 +9,7 @@ public class MainComputerUIManager : MonoBehaviour
     public GameObject MainMenu_UI; // 메인 화면
 
     /* 메인화면 목록*/
-    public GameObject Journal_UI; // 연구 일지 목록
+    public GameObject JournalMenu_UI; // 연구 일지 목록
     public GameObject MeteorList_UI; // 목표 성분 리스트
     public GameObject DogProfile_UI; // 강아지 현재 상태
     public GameObject Wireless_UI; // 블루투스
@@ -29,7 +29,11 @@ public class MainComputerUIManager : MonoBehaviour
         MainMenu_UI.SetActive(true); // 메인 메뉴가 먼저 보이게
         HighBar_UI.SetActive(true); // 상단바 상시 고정
 
-        Journal_UI.SetActive(false);
+        JournalMenu_UI.SetActive(false);
+        MeteorList_UI.SetActive(false);
+        DogProfile_UI.SetActive(false);
+        Wireless_UI.SetActive(false);
+
         Profile_UI.SetActive(false);
         Report_UI.SetActive(false);
         Record_UI.SetActive(false);
@@ -45,44 +49,45 @@ public class MainComputerUIManager : MonoBehaviour
     public void ChangeJournal() // 메인 -> 연구일지 목록
     {
         MainMenu_UI.SetActive(false);
-        Journal_UI.SetActive(true);
+        JournalMenu_UI.SetActive(true);
     }
-
     public void BackMain_Journal() // 연구일지 목록 -> 메인
     {
         MainMenu_UI.SetActive(true);
-        Journal_UI.SetActive(false);
+        JournalMenu_UI.SetActive(false);
     }
+
 
     public void ChangeMeteorList() // 메인 -> 목표 성분 리스트
     {
         MainMenu_UI.SetActive(false);
         MeteorList_UI.SetActive(true);
     }
-
     public void BackMain_MeteorList() // 목표 성분 리스트 -> 메인
     {
         MainMenu_UI.SetActive(true);
         MeteorList_UI.SetActive(false);
     }
-    public void ChangeDogProfile() // 메인 -> 목표 성분 리스트
+
+
+    public void ChangeDogProfile() // 메인 -> 강아지 현재 상태
     {
         MainMenu_UI.SetActive(false);
         DogProfile_UI.SetActive(true);
     }
-
-    public void BackMain_DogProfile() // 목표 성분 리스트 -> 메인
+    public void BackMain_DogProfile() // 강아지 현재 상태 -> 메인
     {
         MainMenu_UI.SetActive(true);
         DogProfile_UI.SetActive(false);
     }
-    public void ChangeBluetooth() // 메인 -> 목표 성분 리스트
+
+
+    public void ChangeBluetooth() // 메인 -> 블루투스
     {
         MainMenu_UI.SetActive(false);
         Wireless_UI.SetActive(true);
     }
-
-    public void BackMain_Bluetooth() // 목표 성분 리스트 -> 메인
+    public void BackMain_Bluetooth() // 블루투스 -> 메인
     {
         MainMenu_UI.SetActive(true);
         Wireless_UI.SetActive(false);
@@ -95,37 +100,36 @@ public class MainComputerUIManager : MonoBehaviour
     /* 연구일지 */
     public void ChangeProfile() // 연구일지 -> 우주 비행사 프로필
     {
-        Journal_UI.SetActive(false);
+        JournalMenu_UI.SetActive(false);
         Profile_UI.SetActive(true);
     }
-
     public void BackJournal_Profile() // 우주 비행사 프로필 -> 연구일지
     {
-        Journal_UI.SetActive(true);
+        JournalMenu_UI.SetActive(true);
         Profile_UI.SetActive(false);
     }
 
+
     public void ChangeReport() // 연구일지 -> 연구 보고서
     {
-        Journal_UI.SetActive(false);
+        JournalMenu_UI.SetActive(false);
         Report_UI.SetActive(true);
     }
-
     public void BackJournal_Report() // 연구 보고서 -> 연구일지
     {
-        Journal_UI.SetActive(true);
+        JournalMenu_UI.SetActive(true);
         Report_UI.SetActive(false);
     }
 
+
     public void ChangeRecord() // 연구일지 -> 녹음 파일
     {
-        Journal_UI.SetActive(false);
+        JournalMenu_UI.SetActive(false);
         Record_UI.SetActive(true);
     }
-
     public void BackJournal_Record() // 녹음 파일 -> 연구일지
     {
-        Journal_UI.SetActive(true);
+        JournalMenu_UI.SetActive(true);
         Record_UI.SetActive(false);
     }
 }
