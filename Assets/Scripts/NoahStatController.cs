752395ed35a9479ab088c25300c1a8b8051b114d
here0jang <63058513+here0jang@users.noreply.github.com>
2022-05-09 01:54:56 +0900

    스탯 애니메이션 추가

--- Assets/Scripts/NoahStatController.cs
+++ Assets/Scripts/NoahStatController.cs
@@ -6,66 +6,210 @@ using UnityEngine.UI;
 public class NoahStatController : MonoBehaviour
 {
     public static NoahStatController noahStatController { get; private set; }
-
+    public int currentNum;
 
     [SerializeField] Image[] statColor;
     [SerializeField] Image[] statBar;
 
     [SerializeField] TMPro.TextMeshProUGUI conditionText;
-    [SerializeField] 
+    float blueTimeLeft = 0f;
+    float redTimeLeft = 0f;
+    float yellowTimeLeft = 0f;
 
-    private int statColorIndex = 3;
-    private int statBarIndex = 9;
+    float maxTime = 1f;
+
+    private float timer = 0f;
+    float conditionTimer = 0f;
+
+    public GameObject statPanel;
+
+    private void Awake()
+    {
+        noahStatController = this;
+    }
     
 
     void Start()
     {
-        StartCoroutine("StatBar");
-        conditionText.transform.gameObject.SetActive(true);
-    }
-        
+        currentNum = GameManager.gameManager._gameData.statNum;
 
-    IEnumerator StatBar()
-    {
-        while (statBarIndex >= 0)
+        for (int i = 0; i < currentNum; i++)
         {
-            Color color = statBar[statBarIndex].GetComponent<Image>().color;
-            color.a = 0f;
-            statBar[statBarIndex].GetComponent<Image>().color = color;
+            statBar[i].enabled = true;
+        }
 
-
-            if(statBarIndex <= 4 && statBarIndex > 1)
-            {
-                Color statColor2 = statColor[2].GetComponent<Image>().color;
-                statColor2.a = 0f;
-                statColor[2].GetComponent<Image>().color = statColor2;
-
-                Color statColor1 = statColor[1].GetComponent<Image>().color;
-                statColor1.a = 1f;
-                statColor[1].GetComponent<Image>().color = statColor1;
-                conditionText.text = "[상태] \"불안함\"";
-            }
-            else if(statBarIndex == 1)
-            {
-                Color statColor1 = statColor[1].GetComponent<Image>().color;
-                statColor1.a = 0f;
-                statColor[1].GetComponent<Image>().color = statColor1;
-
-                Color statColor0 = statColor[0].GetComponent<Image>().color;
-                statColor0.a = 1f;
-                statColor[0].GetComponent<Image>().color = statColor0;
-                conditionText.text = "[상태] \"나쁨\"";
-            }
-            else if(statBarIndex==0)
-            {
-                Color statColor0 = statColor[0].GetComponent<Image>().color;
-                statColor0.a = 0f;
-                statColor[0].GetComponent<Image>().color = statColor0;
-                conditionText.text = "[상태] \"사망\"";
-            }
-            statBarIndex--;
-            GameManager.gameManager._gameData.statNum-=10;
-            yield return new WaitForSeconds(1f); 
+        for (int j = currentNum; j < 10; j++)
+        {
+            statBar[j].enabled = false;
         }
     }
+
+    void TurnOffStatPanel()
+    {
+        statPanel.SetActive(true);
+        conditionTimer += Time.deltaTime;
+        int conditionSeconds = Mathf.FloorToInt((timer % 3600) % 60); // 초 단위 체크
+        if (conditionSeconds > 2)
+        {
+            statPanel.SetActive(false);
+            //conditionTimer = 0;
+        }
+    }
+
+    private void Update()
+    {
+        DecreaseStatByTime();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(GameManager.gameManager._gameData.statNum<10)
+            {
+                ResetStatBar();
+            }          
+        }  
+        
+        if(currentNum<10 && currentNum>=5)
+        {
+            
+            yellowTimeLeft = 0;
+            redTimeLeft = 0;
+
+            statColor[1].fillAmount = 0;
+            statColor[2].fillAmount = 0;
+            if (blueTimeLeft<1)
+            {
+                blueTimeLeft += Time.deltaTime;
+                statColor[0].fillAmount = blueTimeLeft / maxTime;
+            }
+
+            conditionText.text = "[상태] \"좋음\"";
+        }
+
+        if(currentNum < 5 && currentNum >=1)
+        {
+
+            redTimeLeft = 0;
+            blueTimeLeft = 0;
+
+            statColor[0].fillAmount = 0;
+            statColor[2].fillAmount = 0;
+
+            if (yellowTimeLeft < 1)
+            {
+                yellowTimeLeft += Time.deltaTime;
+                statColor[1].fillAmount = yellowTimeLeft / maxTime;
+            }
+
+            conditionText.text = "[상태] \"불안함\"";
+        }
+
+        if(currentNum ==0)
+        {
+
+            yellowTimeLeft = 0;
+            blueTimeLeft = 0;
+
+            statColor[0].fillAmount = 0;
+            statColor[1].fillAmount = 0;
+
+            if (redTimeLeft < 1)
+            {
+                redTimeLeft += Time.deltaTime;
+                statColor[2].fillAmount = redTimeLeft / maxTime;
+            }
+
+            conditionText.text = "[상태] \"사망\"";
+        }
+    }
+    
+    public void ResetStatBar()
+    {
+
+        currentNum = GameManager.gameManager._gameData.statNum;
+        currentNum += 1;
+        GameManager.gameManager._gameData.statNum = currentNum;
+        SaveSystem.Save(GameManager.gameManager._gameData, "save_001");
+
+        for (int i = 0; i < currentNum; i++)
+        {
+            statBar[i].enabled = true;
+        }
+
+        for (int j = currentNum; j< 10; j++)
+        {
+            statBar[j].enabled = false;
+        }
+    }
+
+    void  DecreaseStatByTime()
+    {
+        if(currentNum>=1)
+        {
+            timer += Time.deltaTime;
+            int seconds = Mathf.FloorToInt((timer % 3600) % 60); // 초 단위 체크
+            if (seconds >= 2)
+            {
+                currentNum = GameManager.gameManager._gameData.statNum;
+                currentNum -= 1;
+
+                for (int i = 0; i < currentNum; i++)
+                {
+                    statBar[i].enabled = true;
+                }
+
+                for (int j = currentNum; j < 10; j++)
+                {
+                    statBar[j].enabled = false;
+                }
+                GameManager.gameManager._gameData.statNum = currentNum;
+                SaveSystem.Save(GameManager.gameManager._gameData, "save_001");
+                timer = 0;
+            }
+        }
+    }
+
+
+    //IEnumerator StatBar()
+    //{
+    //    while (statBarIndex >= 0)
+    //    {
+    //        Color color = statBar[statBarIndex].GetComponent<Image>().color;
+    //        color.a = 0f;
+    //        statBar[statBarIndex].GetComponent<Image>().color = color;
+
+
+    //        if(statBarIndex <= 4 && statBarIndex > 1)
+    //        {
+    //            Color statColor2 = statColor[2].GetComponent<Image>().color;
+    //            statColor2.a = 0f;
+    //            statColor[2].GetComponent<Image>().color = statColor2;
+
+    //            Color statColor1 = statColor[1].GetComponent<Image>().color;
+    //            statColor1.a = 1f;
+    //            statColor[1].GetComponent<Image>().color = statColor1;
+    //            conditionText.text = "[상태] \"불안함\"";
+    //        }
+    //        else if(statBarIndex == 1)
+    //        {
+    //            Color statColor1 = statColor[1].GetComponent<Image>().color;
+    //            statColor1.a = 0f;
+    //            statColor[1].GetComponent<Image>().color = statColor1;
+
+    //            Color statColor0 = statColor[0].GetComponent<Image>().color;
+    //            statColor0.a = 1f;
+    //            statColor[0].GetComponent<Image>().color = statColor0;
+    //            conditionText.text = "[상태] \"나쁨\"";
+    //        }
+    //        else if(statBarIndex==0)
+    //        {
+    //            Color statColor0 = statColor[0].GetComponent<Image>().color;
+    //            statColor0.a = 0f;
+    //            statColor[0].GetComponent<Image>().color = statColor0;
+    //            conditionText.text = "[상태] \"사망\"";
+    //        }
+    //        statBarIndex--;
+    //        GameManager.gameManager._gameData.statNum-=10;
+    //        yield return new WaitForSeconds(1f); 
+    //    }
+    //}
 }
