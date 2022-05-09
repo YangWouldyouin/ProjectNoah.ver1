8c66fcdd59462629a1311fdfb6fa8d4a528d57a6
YangWouldyouin <92512710+YangWouldyouin@users.noreply.github.com>
2022-05-10 02:57:34 +0900

    다운로드 퍼센트 추가~
    게이지는 포기했어요ㅎㅎ

---
+++ Assets/Scripts/UIManager/TW_UploadUIManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TW_UploadUIManager : MonoBehaviour
+{
+    public Text downloadingText;
+    public GameObject UploadPage;
+
+    float setTime = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(UploadPage.activeSelf == true)
+        {
+            Debug.Log("dd");
+            downloadingText.text = "FileUploading " + 0 + "%";
+
+            if (setTime < 100)
+            {
+                setTime += Time.deltaTime * 70;
+            }
+
+            downloadingText.text = "FileUploading " + Mathf.Round(setTime).ToString() + "%";
+        }
+        else if(UploadPage.activeSelf == false)
+        {
+            Debug.Log("ss");
+            setTime = 0;
+            downloadingText.text = "FileUploading " + 0 + "%";
+        }
+    }
+}
