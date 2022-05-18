bc7b0ac9fad1ba3b23e8b0ef31ca8591548be405
YangWouldyouin <92512710+YangWouldyouin@users.noreply.github.com>
2022-05-18 16:23:09 +0900

    엔딩 기능 추가!!!!!!!!!!!!!!!!!!!!!

--- Assets/Scripts/UIManager/SuddenDeathAlertUIManager.cs
+++ Assets/Scripts/UIManager/SuddenDeathAlertUIManager.cs
@@ -6,54 +6,68 @@ using UnityEngine.UI;
 public class SuddenDeathAlertUIManager : MonoBehaviour
 {
     public GameObject DeathAlert;
-    public Text NumCountText;
+    //public Text NumCountText;
+
+    [SerializeField] float setTime = 5.0f;
+    [SerializeField] Text NumCountText;
 
     void Start()
     {
-
+        NumCountText.text = setTime.ToString();
     }
-
+    
     void Update()
     {
         if (GameManager.gameManager._gameData.IsSuddenDeath == true)
         {
-            NumCountText.GetComponent<Text>().text = "5";
-            Invoke("Count4", 1f);
+            DeathAlert.SetActive(true);
+            
+            if(setTime > 0)
+            {
+                setTime -= Time.deltaTime;
+            }
+            else if (setTime <=0)
+            {
+                CountEnd();
+            }
+
+            NumCountText.text = Mathf.Round(setTime).ToString();
         }
         else
         {
             DeathAlert.SetActive(false);
+            setTime = 5.0f;
         }
     }
 
 
-    public void Count4()
-    {
-        NumCountText.GetComponent<Text>().text = "4";
-        Invoke("Count3", 1f);
-    }
-    public void Count3()
-    {
-        NumCountText.GetComponent<Text>().text = "3";
-        Invoke("Count2", 1f);
-    }
+    //public void Count4()
+    //{
+    //    NumCountText.GetComponent<Text>().text = "4";
+    //    Invoke("Count3", 1f);
+    //}
+    //public void Count3()
+    //{
+    //    NumCountText.GetComponent<Text>().text = "3";
+    //    Invoke("Count2", 1f);
+    //}
 
-    public void Count2()
-    {
-        NumCountText.GetComponent<Text>().text = "2";
-        Invoke("Count1", 1f);
-    }
+    //public void Count2()
+    //{
+    //    NumCountText.GetComponent<Text>().text = "2";
+    //    Invoke("Count1", 1f);
+    //}
 
-    public void Count1()
-    {
-        NumCountText.GetComponent<Text>().text = "1";
-        Invoke("Count0", 1f);
-    }
-    public void Count0()
-    {
-        NumCountText.GetComponent<Text>().text = "0";
-        Invoke("CountEnd", 1f);
-    }
+    //public void Count1()
+    //{
+    //    NumCountText.GetComponent<Text>().text = "1";
+    //    Invoke("Count0", 1f);
+    //}
+    //public void Count0()
+    //{
+    //    NumCountText.GetComponent<Text>().text = "0";
+    //    Invoke("CountEnd", 1f);
+    //}
 
     public void CountEnd()
     {
