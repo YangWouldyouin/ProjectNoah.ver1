19990e54b24029ffa19aa6b50c6d93ff9c02a78c
YangWouldyouin <92512710+YangWouldyouin@users.noreply.github.com>
2022-05-25 16:07:58 +0900

    빌드 창모드 실험...
    실패...

--- Assets/Scripts/UIManager/SplashUIManager.cs
+++ Assets/Scripts/UIManager/SplashUIManager.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class SplashUIManager : MonoBehaviour
 {
     public GameObject Text;
-    
+
+    public bool Fullscreenon = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        setResolution();
+        //setResolution();
     }
 
     // Update is called once per frame
@@ -20,6 +22,26 @@ public class SplashUIManager : MonoBehaviour
         {
             SceneManager.LoadScene("Main");
         }
+
+        //if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    if(Fullscreenon == true)
+        //    {
+        //        int setWidth = 1440;
+        //        int setHeight = 900;
+
+        //        Screen.SetResolution(setWidth, setHeight, false);
+        //        Fullscreenon = false;
+        //    }
+        //    else if (Fullscreenon == false)
+        //    {
+        //        int setWidth = 1920;
+        //        int setHeight = 1080;
+
+        //        Screen.SetResolution(setWidth, setHeight, true);
+        //        Fullscreenon = true;
+        //    }
+        //}
     }
 
     public void setResolution()
