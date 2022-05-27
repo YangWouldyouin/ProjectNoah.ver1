2f513934963bdb6c60761b4e1e3adbdb45c28068
here0jang <63058513+here0jang@users.noreply.github.com>
2022-05-27 09:40:43 +0900

    수정

--- Assets/NavMeshBaker.cs
+++ Assets/NavMeshBaker.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class NavMeshBaker : MonoBehaviour
 {
-    public NavMeshSurface[] surfaces;
+    //public NavMeshSurface[] surfaces;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,7 @@ public class NavMeshBaker : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            surfaces[1].BuildNavMesh();
+            //surfaces[1].BuildNavMesh();
         }
     }
 }
