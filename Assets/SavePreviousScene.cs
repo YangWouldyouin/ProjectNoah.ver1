fa34f0a9d2b4a29cb1253cd325c519584d3db91c
here0jang <63058513+here0jang@users.noreply.github.com>
2022-05-30 02:35:46 +0900

    수정

--- Assets/SavePreviousScene.cs
+++ Assets/SavePreviousScene.cs
@@ -5,21 +5,41 @@ using UnityEngine;
 public class SavePreviousScene : MonoBehaviour
 {
 
-    public static string PreviousLevel { get; private set; }
+    public static int PreviousLevel { get; private set; }
     GameObject noah;
 
     private void OnDestroy()
     {
-        PreviousLevel = gameObject.scene.name;
+        PreviousLevel = gameObject.scene.buildIndex;
     }
 
     private void Start()
     {
         noah = BaseCanvas._baseCanvas.noahPlayer;
-        if(PreviousLevel=="new engineRoom")
+        
+        if(PreviousLevel==3)
         {
             noah.transform.position = new Vector3(-261.13f, 0.915751f, 669.4411f);
             noah.transform.eulerAngles = new Vector3(0, 0, 0);
         }
+        
+
+        switch(PreviousLevel)
+        {
+            case 3: // 엔진실이면 
+                {
+                    noah.transform.position = new Vector3(-261.13f, 0.915751f, 669.4411f);
+                    noah.transform.eulerAngles = new Vector3(0, 0, 0);
+                    break;
+                }
+            case 5: // 생활공간이면
+                {
+                    noah.transform.position = new Vector3(-261.13f, 0.915751f, 691.5332f);
+                    noah.transform.eulerAngles = new Vector3(0, 180, 0);
+                    break;
+                }
+        }
+
+
     }
 }
