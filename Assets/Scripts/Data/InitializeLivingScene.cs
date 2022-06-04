ae6a7c3050a2fb6e4b44f4a58edbd2b22db3c41a
here0jang <63058513+here0jang@users.noreply.github.com>
2022-06-04 20:59:19 +0900

    기계 수리 수정

--- Assets/Scripts/Data/InitializeLivingScene.cs
+++ Assets/Scripts/Data/InitializeLivingScene.cs
@@ -33,9 +33,6 @@ public class InitializeLivingScene : MonoBehaviour
         // 엔진실 냄새 기계를 고치면
         if (intialGameData.IsFuelabsorberFixed_E_E1)
         {
-            TDBT_fixPart.GetComponent<Rigidbody>().isKinematic = false;
-            TDBT_fixPart.transform.parent = null;
-
             TDBT_fixPart.transform.position = new Vector3(-27.253f, 1.844f, 35.729f);
             TDBT_fixPart.transform.rotation = Quaternion.Euler(0, -90, 0);
             TDBT_fixPart.transform.localScale = new Vector3(50f, 50.00002f, 50.00002f);
